bcd9ab908da8ebdab3ffb73939eba579328f2ad1
Sean Li <lxin618@aucklanduni.ac.nz>
2024-06-26 10:22:57 +1200

    update tf & gthub action

--- iac/vars.tf
+++ iac/vars.tf
@@ -1,12 +1,16 @@
 variable "elasticapp" {
+  type = string
   default = "healthe-staging"
 }
 variable "beanstalkappenv" {
+  type = string
   default = "Healthe-staging-env"
 }
 variable "solution_stack_name" {
   type = string
+  default = "Node.js 20 running on 64bit Amazon Linux 2023"
 }
 variable "tier" {
   type = string
+  default = "WebServer"
 }
